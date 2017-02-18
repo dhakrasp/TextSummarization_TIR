ca6598fe65b108fcf108da879bb66fa5fbaf906e
dhakrasp <pranav.dhakras@gmail.com>
2017-02-18 14:31:21 +0530

    work in progress...

--- generate_vocab.py
+++ generate_vocab.py
@@ -4,34 +4,46 @@ import codecs
 import re
 import xml.etree.ElementTree as et
 
+UNKNOWN_TOKEN = '<UNK>'
+
+
 def generate_vocab(data_dir_name):
-	word_counts = dict()
-	for fname in os.listdir(data_dir_name):
-		for line in codecs.open(os.path.join(data_dir_name, fname), encoding = 'utf-8'):
-			tokens = nltk.word_tokenize(line)
-			for t in tokens:
-				tok = t.lower()
-				if word_counts.has_key(tok):
-					word_counts[tok] = word_counts[tok] + 1
-				else:
-					word_counts[tok] = 1
-	return word_counts
+    word_counts = dict()
+    word_counts[UNKNOWN_TOKEN] = 0
+    for fname in os.listdir(data_dir_name):
+        for line in codecs.open(os.path.join(data_dir_name, fname), encoding='utf-8'):
+            tokens = nltk.word_tokenize(line)
+            for t in tokens:
+                tok = t.lower()
+                if tok in word_counts.keys():
+                    word_counts[tok] = word_counts[tok] + 1
+                else:
+                    word_counts[tok] = 1
+    return word_counts
+
 
 def convert_to_raw_text(data_dir_name):
-	for fname in os.listdir(data_dir_name):
-		with codecs.open(os.path.join(data_dir_name, fname), encoding = 'utf-8') as f:
-			xml_data = f.read()
-			root = et.fromstring(xml_data)
-			text = root.find('content').text
-			re.sub('<[^<]+?>', '', text)
-			with codecs.open(os.path.join(data_dir_name, fname), mode = 'w', encoding = 'utf-8') as raw:
-				raw.write(text)
-			
+    for fname in os.listdir(data_dir_name):
+        with codecs.open(os.path.join(data_dir_name, fname), encoding='utf-8') as f:
+            xml_data = f.read()
+            root = et.fromstring(xml_data)
+            text = root.find('content').text
+            re.sub('<[^<]+?>', '', text)
+            with codecs.open(os.path.join(data_dir_name, fname), mode='w', encoding='utf-8') as raw:
+                raw.write(text)
+
+
+def write_vocab(data_dir_name, vocab_file_name):
+    word_counts = generate_vocab(data_dir_name)
+    with codecs.open(vocab_file_name, mode='w', encoding='utf-8') as f:
+        for k, v in word_counts.items():
+            f.write(u'{}\t{}\n'.format(k, v))
+
 
 if __name__ == '__main__':
-	data_dir_name = 'raw_data'
-	vocab_file_name = 'raw_data/vocab'
-	word_counts = generate_vocab(data_dir_name)
-	with codecs.open(vocab_file_name, mode = 'a', encoding = 'utf-8') as f:
-		for k,v in word_counts.items():
-			f.write(u'{}\t{}\n'.format(k, v))
+    data_dir_name = 'target'
+    vocab_file_name = 'vocab/tar_vocab'
+    write_vocab(data_dir_name, vocab_file_name)
+    data_dir_name = 'source'
+    vocab_file_name = 'vocab/src_vocab'
+    write_vocab(data_dir_name, vocab_file_name)
--- wip.py
+++ wip.py
@@ -1,151 +1,190 @@
-'''Trains a simple convnet on the MNIST dataset.
-Gets to 99.25% test accuracy after 12 epochs
-(there is still a lot of margin for parameter tuning).
-16 seconds per epoch on a GRID K520 GPU.
-'''
-
 from __future__ import print_function
-import os
-os.environ['KERAS_BACKEND'] = 'tensorflow'
+# os.environ['KERAS_BACKEND'] = 'theano'
 import numpy as np
-from keras.preprocessing.text import Tokenizer
+import codecs
+import json
+# from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
-from keras.models import Sequential
+from keras.models import Sequential, model_from_json
 from keras.layers import Dense, Activation, Dropout, Embedding, TimeDistributed, RepeatVector
-from keras.layers import LSTM
+from keras.layers import LSTM, GRU, SimpleRNN
+from keras.callbacks import ModelCheckpoint
+from timeit import default_timer as timer
+from Tokenizer import Tokenizer
+from data import Vocab
 
 
-def shift(seq, n):
-    n = n % len(seq)
-    return seq[n:] + seq[:n]
+MAX_SRC_LEN = 60
+MAX_TAR_LEN = 20
 
 
-def decode(prediction, target_dict):
+def decode(prediction, vocab):
     outputs = []
     for i in range(0, prediction.shape[0]):
         sample = []
         e = prediction[i]
-        # s = np.sum(e, axis=1)
-        # for j in range(s.shape[0]):
-        # 	e[j] = e[j]/s[j]
-        # max_args = np.argmax(e, axis=1)
         for j in range(e.shape[0]):
             k = e[j]
             if k != 0:
-                sample.append(target_dict[k])
+                sample.append(vocab.IdToWord(k))
         outputs.append(sample)
     return outputs
 
 
-def encode(text, tokenizer=None, vocab=None, max_len=10):
-    if tokenizer is None:
-        tk = Tokenizer(nb_words=2000, lower=True, split=" ")
-        tk.fit_on_texts(text)
-    else:
-        tk = tokenizer
-    x = tk.texts_to_sequences(text)
-
-    # enc_data = np.zeros((len(x), max_len, len(tk.word_index)+1), dtype=np.bool)
-    # for i in range(0, len(x)):
-    # 	sample = x[i]
-    # 	for j in range(0, len(sample)):
-    # 		k = sample[j] -1
-    # 		enc_data[i][j][k] = 1
-    return [x, tk]
+def encode_input(text, tokenizer, MAX_SRC_LEN):
+    assert tokenizer is not None
+    assert MAX_SRC_LEN > 0
+    return tokenizer.texts_to_sequences(text)
 
 
-def encode_output(text, vocab=None, max_len=10):
-    tk = Tokenizer(nb_words=2000, lower=True, split=" ")
-    tk.fit_on_texts(text)
-    x = tk.texts_to_sequences(text)
-
+def encode_output(text, tokenizer, MAX_TAR_LEN):
+    assert tokenizer is not None
+    assert MAX_TAR_LEN > 0
+    x = tokenizer.texts_to_sequences(text)
     enc_data = np.zeros(
-        (len(x), max_len, len(tk.word_index) + 1), dtype=np.bool)
+        (len(x), MAX_TAR_LEN, tokenizer.vocab.NumIds() + 1), dtype=np.bool)
     for i in range(0, len(x)):
         sample = x[i]
         for j in range(0, len(sample)):
             k = sample[j]
             enc_data[i][j][k] = 1
-        for j in range(len(sample), max_len):
+        for j in range(len(sample), MAX_TAR_LEN):
             k = 0
             enc_data[i][j][k] = 1
-    return [enc_data, tk]
+    return enc_data
 
 
-# def preprocess_data(text, vocab):
-#     data = encode(text, vocab)
+def preprocess_data(src_text, src_tokenizer, tar_text=None, tar_tokenizer=None):
+    assert src_tokenizer is not None
+    assert src_txt is not None
+    src_data = encode_input(src_text, src_tokenizer, MAX_SRC_LEN)
+    Y = None
+    if tar_text is not None:
+        assert tar_tokenizer is not None
+        Y = encode_output(tar_text, tar_tokenizer, MAX_TAR_LEN)
+    X = pad_sequences(src_data, maxlen=MAX_SRC_LEN, padding='post')
+    return [X, Y]
 
 
-def train(src_txt, tar_txt, model_file, num_layers=2):
+def build_graph(hyper_params, model_file):
 
-    src_data, src_tk = encode(src_txt)
-    tar_data, tar_tk = encode_output(tar_txt)
-    src_max_len = 10
-    tar_max_len = 10
-    X = pad_sequences(src_data, maxlen=src_max_len, padding='post')
-    # Y = pad_sequences(tar_data, maxlen=tar_max_len, padding='post')
-    Y = tar_data
+    input_dim = hyper_params['input_dim']
+    embedding_dim = hyper_params['embedding_dim']
+    hidden_dim = hyper_params['hidden_dim']
+    output_dim = hyper_params['output_dim']
+    batch_size = hyper_params['batch_size']
+    num_layers = hyper_params['num_layers']
+    max_src_len = hyper_params['max_src_len']
+    max_tar_len = hyper_params['max_tar_len']
+    str_cell_type = hyper_params['cell_type']
 
-    # padding sequence
-    max_len = 10
-    input_dim = len(src_tk.word_counts) + 1
-    embedding_dim = 50
-    hidden_dim = 10
-    output_dim = input_dim
-    batch_size = 20
+    # LSTM is the default type
+    if str_cell_type == 'SimpleRNN':
+        cell_type = SimpleRNN
+    elif str_cell_type == 'GRU':
+        cell_type = GRU
+    else:
+        cell_type = LSTM
+
     # lstm model
     model = Sequential()
-    model.add(Embedding(input_dim + 1, embedding_dim, init = 'lecun_uniform', dropout=0.2,
-                        input_shape=(max_len, input_dim), mask_zero=True))
+    model.add(Embedding(input_dim + 1, embedding_dim, init='lecun_uniform', dropout=0.2,
+                        input_shape=(max_src_len, input_dim), mask_zero=True))
     RNNCell = LSTM
-    model.add(RNNCell(output_dim, dropout_W=0.2, dropout_U=0.2))
-    model.add(RepeatVector(max_len))
+
+    # This is the encoder
+    model.add(RNNCell(hidden_dim, dropout_W=0.2, dropout_U=0.2))
+    model.add(Dense(hidden_dim))
+    # Feed the Hidden state to each time step of decoder
+    model.add(RepeatVector(max_tar_len))
+
+    # This is the decoder
     for i in range(num_layers):
-        model.add(RNNCell(output_dim, return_sequences=True,
+        model.add(RNNCell(hidden_dim, return_sequences=True,
                           dropout_W=0.2, dropout_U=0.2))
     model.add(TimeDistributed(Dense(output_dim)))
     model.add(Activation('softmax'))
+
     model.compile(loss='binary_crossentropy', optimizer='rmsprop')
+    model_json = model.to_json()
+    print(model_json)
+    with open(model_file, mode='w') as f:
+        json.dump(model_json, f)
     model.summary()
-
-    model.fit(X, Y, batch_size=batch_size, nb_epoch=50)
-    return [model, src_tk, tar_tk]
+    return model
 
 
-def test(txt, tokenizer, target_dict, model, vocab=None):
-    print('\n' + '-' * 50 + 'testing' + '-' * 50 + '\n')
-    test_data, test_tk = encode(txt, tokenizer=tokenizer)
-    tar_max_len = 10
+def train(X, Y, hyper_params, epochs, model_file, weights_file):
+    model = build_graph(hyper_params, model_file)
+    batch_size = hyper_params['batch_size']
+    checkpoint_callback = ModelCheckpoint(
+        weights_file, monitor='val_loss', verbose=0, save_best_only=False, save_weights_only=False, mode='auto', period=1)
+    start = timer()
+    model.fit(X, Y, batch_size=batch_size, callbacks=[
+              checkpoint_callback], nb_epoch=epochs)
+    end = timer()
+    print('\n\n\nTime to train\t{}\t{}s'.format('-' * 50 + '>', end - start))
+    return [model, src_tokenizer, tar_tokenizer]
+
+
+def test(txt, tokenizer, target_vocab, model):
+    X, _ = preprocess_data(txt, tokenizer, tar_text=None, tar_tokenizer=None)
     batch_size = 20
-    X = pad_sequences(test_data, maxlen=tar_max_len, padding='post')
 
     pred = model.predict_classes(X, batch_size=batch_size)
-    print(pred)
+    # print(pred)
 
-    outputs = decode(pred, target_dict)
+    outputs = decode(pred, target_vocab)
+    print('\n\n\n')
+    print('-' * 50 + '\t\tTesting\t\t' + '-' * 50)
     print(outputs)
-    print('-' * 100)
+
+
+def get_file_content(file_name):
+    with codecs.open(file_name, mode='r', encoding='utf-8') as f:
+        return f.read()
 
 
 if __name__ == '__main__':
     src_txt = []
-    for i in range(700):
-        src_txt.append("Hi, this is Pooojitha and Pranav.")
-        src_txt.append("Hi, this is Pranav and Pooojitha.")
+    src_file_name = "source/50"
+    tar_file_name = "target/10"
+    for i in range(100):
+        src_txt.append(get_file_content(src_file_name))
     tar_txt = []
-    for i in range(700):
-        tar_txt.append("gi, ghis gis Goojitha and Granav.")
-        tar_txt.append("gi, ghis gis Granav and Goojitha.")
+    for i in range(100):
+        tar_txt.append(get_file_content(tar_file_name))
 
-    # vocab = Vocab('raw_data/vocab')
-    model, src_tk, tar_tk = train(src_txt, tar_txt, 'model')
+    src_tokenizer = Tokenizer(Vocab('vocab/src_vocab', 10000))
+    tar_tokenizer = Tokenizer(Vocab('vocab/tar_vocab', 10000))
+    X, Y = preprocess_data(src_txt, src_tokenizer, tar_txt, tar_tokenizer)
 
-    tar_index_to_word = {}
-    for k, v in tar_tk.word_index.items():
-        tar_index_to_word[v] = k
+    input_dim = src_tokenizer.vocab.NumIds() + 1
+    embedding_dim = 100
+    hidden_dim = 50
+    output_dim = tar_tokenizer.vocab.NumIds() + 1
+    batch_size = 20
+    num_layers = 2
+    epochs = 20
+    cell_type = 'LSTM'
+
+    hyper_params = {}
+    hyper_params['input_dim'] = input_dim
+    hyper_params['embedding_dim'] = embedding_dim
+    hyper_params['hidden_dim'] = hidden_dim
+    hyper_params['output_dim'] = output_dim
+    hyper_params['batch_size'] = batch_size
+    hyper_params['num_layers'] = num_layers
+    hyper_params['epochs'] = epochs
+    hyper_params['cell_type'] = cell_type
+    hyper_params['max_src_len'] = MAX_SRC_LEN
+    hyper_params['max_tar_len'] = MAX_TAR_LEN
+
+    model, src_tokenizer, tar_tokenizer = train(
+        X, Y, hyper_params, epochs, model_file='model', weights_file='weights')
 
     test_txt = []
     for i in range(1):
-        test_txt.append("Hi, Granav is this and Pooojitha.")
+        test_txt.append(get_file_content(src_file_name))
 
-    test(test_txt, src_tk, tar_index_to_word, model)
+    test(test_txt, src_tokenizer, tar_tokenizer.vocab, model)
